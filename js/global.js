0076699efe22c4635cc742ad8ba9391a5dba31fe
andypsh <andypsh@naver.com>
2022-05-24 20:39:36 +0900

    0524_third

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     scroll_top_btn();
     nav_arrow_btn();
     nav_menu_btn();
@@ -7,47 +7,48 @@ $(document).ready(function(){
     nav_story_active();
 });
 
-function scroll_top_btn(){
-    $('.top_button').on('click',function(){
-        $('html, body').animate({scrollTop: '0'}, 300);
+function scroll_top_btn() {
+    $('.top_button').on('click', function () {
+        $('html, body').animate({
+            scrollTop: '0'
+        }, 300);
     });
 };
 
-function nav_menu_btn(){
-    $('.mobile_icon div span').on('click',function(){
+function nav_menu_btn() {
+    $('.mobile_icon div span').on('click', function () {
         $('nav').addClass('active');
-        $('body').css('overflow','hidden');
+        $('body').css('overflow', 'hidden');
     });
 
-    $('nav ul>li:nth-child(1) span').on('click',function(){
+    $('nav ul>li:nth-child(1) span').on('click', function () {
         $('nav').removeClass('active');
-        $('body').css('overflow','visible');
+        $('body').css('overflow', 'visible');
     });
 };
 
-function nav_arrow_btn(){
-    $('nav ul>li>a span').on('click',function(e){
-        if($(window).width() < 2500){/*navigation 크기 조절*/
+function nav_arrow_btn() {
+    $('nav ul>li>a span').on('click', function (e) {
+        if ($(window).width() < 2500) {
+            /*navigation 크기 조절*/
             e.preventDefault();
         }
     });
 
-    $('nav ul>li>a span').on('click',function(){
+    $('nav ul>li>a span').on('click', function () {
         $(this).parent().next().toggleClass('active');
         $(this).parent().find('span').toggleClass('active');
     });
 };
 
-function footer_arrow_btn(){
-}
+function footer_arrow_btn() {}
 
-function nav_menu_active(){
-    $('nav ul li:nth-child(4) ol li').eq(1).click(function(){
-        $.cookie('cookie_menu','discount');
+function nav_menu_active() {
+    $('nav ul li:nth-child(4) ol li').eq(1).click(function () {
+        $.cookie('cookie_menu', 'discount');
     });
 
-   
+
 }
 
-function nav_story_active(){
-}
+function nav_story_active() {}
